d20cdc69c339106a0a4e678dcb1de6ace22f8a5c
Zan Baldwin <hello@zanbaldwin.com>
2023-12-24 21:15:27 +0100

    Stow: Git functions; better semantic version sorting

--- stow/home/bash/.bashrc.d/git.sh
+++ stow/home/bash/.bashrc.d/git.sh
@@ -4,11 +4,11 @@ export GIT_DISCOVERY_ACROSS_FILESYSTEM=0
 
 # This function relies on Git repositories having sensible tag names. Do not rely on it.
 latest_stable_version () {
-    #local GIT_DIR="${1:-$(pwd)}"
+    GIT_DIR="${1:-$(pwd)}"
     # Fetch all tags that contain at least a MAJOR.MINOR, filtering out hyphens (alpha, beta, rc, etc), and print the latest.
     git -C "${GIT_DIR}" tag --list --sort="version:refname" \
         | rg -e '\d+\.\d+' \
         | rg -v 'alpha|ALPHA|beta|BETA|rc|RC|-' \
-        | sort -r --version-sort \
+        | sort --version-sort \
         | tail -n1
 }

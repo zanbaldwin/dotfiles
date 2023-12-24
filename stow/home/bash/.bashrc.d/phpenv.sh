023cd376e303bb3e1031c7d13f5723937cb5f19e
Zan Baldwin <hello@zanbaldwin.com>
2023-12-24 21:15:28 +0100

    PHPEnv: Don't Load Shell Config File if not exists

--- stow/home/bash/.bashrc.d/phpenv.sh
+++ stow/home/bash/.bashrc.d/phpenv.sh
@@ -1,20 +1,22 @@
 add_to_path "${HOME}/.phpenv/bin"
 add_to_path "${HOME}/.phpenv/shims"
 
-source "${HOME}/.phpenv/completions/phpenv.bash"
-phpenv rehash 2>/dev/null
+if [ -f "${HOME}/.phpenv/completions/phpenv.bash" ]; then
+    source "${HOME}/.phpenv/completions/phpenv.bash"
+    phpenv rehash 2>/dev/null
+fi
 
 phpenv() {
-  local command
-  command="$1"
-  if [ "$#" -gt 0 ]; then
-    shift
-  fi
+    local command
+    command="$1"
+    if [ "$#" -gt 0 ]; then
+      shift
+    fi
 
-  case "$command" in
-  shell)
-    eval `phpenv "sh-$command" "$@"`;;
-  *)
-    command phpenv "$command" "$@";;
-  esac
+    case "$command" in
+    shell)
+      eval `phpenv "sh-$command" "$@"`;;
+    *)
+      command phpenv "$command" "$@";;
+    esac
 }

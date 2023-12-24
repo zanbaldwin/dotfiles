0cdb70704966747b2e8afbc9de349d7c3d8a04dc
Zan Baldwin <hello@zanbaldwin.com>
2023-12-24 21:15:26 +0100

    Ignore Vulkan and xcb-errors for now.

--- toolbox/build-hyprland.sh
+++ toolbox/build-hyprland.sh
@@ -1,66 +1,66 @@
 #!/bin/bash
 
-# xcb-errors isn't available in the Fedora repositories.
-sudo dnf install --assumeyes \
-    autoconf \
-    automake \
-    libxcb-devel \
-    xcb-proto \
-    xorg-x11-util-macros
-git clone --recursive "https://gitlab.freedesktop.org/xorg/lib/libxcb-errors" "/tmp/libxcb-errors"
-command -v "python" >"/dev/null" 2>&1 || { sudo ln -s "/usr/bin/python3" "/usr/bin/python"; }
-( \
-    cd "/tmp/libxcb-errors"; \
-    git -C "/tmp/libxcb-errors" checkout "$(latest_stable_version)"; \
-    bash "/tmp/libxcb-errors/autogen.sh" --prefix="/usr"; \
-    sudo make install; \
-)
+# # xcb-errors isn't available in the Fedora repositories.
+# sudo dnf install --assumeyes \
+#     autoconf \
+#     automake \
+#     libxcb-devel \
+#     xcb-proto \
+#     xorg-x11-util-macros
+# git clone --recursive "https://gitlab.freedesktop.org/xorg/lib/libxcb-errors" "/tmp/libxcb-errors"
+# command -v "python" >"/dev/null" 2>&1 || { sudo ln -s "/usr/bin/python3" "/usr/bin/python"; }
+# ( \
+#     cd "/tmp/libxcb-errors"; \
+#     git -C "/tmp/libxcb-errors" checkout "$(latest_stable_version)"; \
+#     bash "/tmp/libxcb-errors/autogen.sh" --prefix="/usr"; \
+#     sudo make install; \
+# )
 
-# Vulkan Renderer
-sudo dnf install --assumeyes \
-    gcc-c++ \
-    glslang-devel \
-    libfontenc-devel \
-    libuuid-devel \
-    libxkbfile-devel \
-    libxcb-devel \
-    libXrandr-devel \
-    libXres-devel \
-    libXaw-devel \
-    libXcursor-devel \
-    libXcomposite-devel \
-    libXdamage-devel \
-    libXdamage-devel \
-    libXdmcp-devel \
-    libXinerama-devel \
-    libXi-devel \
-    libXtst-devel \
-    libXvMC-devel \
-    libXv-devel \
-    libXxf86vm-devel \
-    libXScrnSaver-devel \
-    libICE-devel \
-    libSM-devel \
-    mesa-libGL-devel \
-    mingw64-vulkan-headers \
-    mingw64-vulkan-loader \
-    ninja-build \
-    pip \
-    vulkan-headers \
-    vulkan-loader-devel \
-    xcb-util-devel \
-    xcb-util-image-devel \
-    xcb-util-keysyms-devel \
-    xcb-util-renderutil-devel \
-    xcb-util-wm-devel \
-    xkeyboard-config-devel \
-    xorg-x11-xtrans-devel
-pip install \
-    conan \
-    setuptools \
-    wheel
-git clone --recursive "https://github.com/inexorgame/vulkan-renderer" "/tmp/vulkan-renderer"
-(cd "/tmp/vulkan-renderer"; cmake . -Bbuild -D'CMAKE_BUILD_TYPE=Debug' -G'Ninja' && ninja -C build)
+# # Vulkan Renderer
+# sudo dnf install --assumeyes \
+#     gcc-c++ \
+#     glslang-devel \
+#     libfontenc-devel \
+#     libuuid-devel \
+#     libxkbfile-devel \
+#     libxcb-devel \
+#     libXrandr-devel \
+#     libXres-devel \
+#     libXaw-devel \
+#     libXcursor-devel \
+#     libXcomposite-devel \
+#     libXdamage-devel \
+#     libXdamage-devel \
+#     libXdmcp-devel \
+#     libXinerama-devel \
+#     libXi-devel \
+#     libXtst-devel \
+#     libXvMC-devel \
+#     libXv-devel \
+#     libXxf86vm-devel \
+#     libXScrnSaver-devel \
+#     libICE-devel \
+#     libSM-devel \
+#     mesa-libGL-devel \
+#     mingw64-vulkan-headers \
+#     mingw64-vulkan-loader \
+#     ninja-build \
+#     pip \
+#     vulkan-headers \
+#     vulkan-loader-devel \
+#     xcb-util-devel \
+#     xcb-util-image-devel \
+#     xcb-util-keysyms-devel \
+#     xcb-util-renderutil-devel \
+#     xcb-util-wm-devel \
+#     xkeyboard-config-devel \
+#     xorg-x11-xtrans-devel
+# pip install \
+#     conan \
+#     setuptools \
+#     wheel
+# git clone --recursive "https://github.com/inexorgame/vulkan-renderer" "/tmp/vulkan-renderer"
+# (cd "/tmp/vulkan-renderer"; cmake . -Bbuild -D'CMAKE_BUILD_TYPE=Debug' -G'Ninja' && ninja -C build)
 
 # Hyprland
 sudo dnf install --assumeyes \
@@ -96,11 +96,9 @@ sudo dnf install --assumeyes \
     xcb-util-wm-devel \
     xorg-x11-server-Xwayland-devel
 
-git clone --recursive "https://github.com/hyprwm/Hyprland.git" "/tmp/hyprland"
-# "/usr/local" is the only mutable location we can install system libraries and shares on Silverblue.
-(cd "/tmp/hyprland"; sudo make install PREFIX="/usr/local")
+git clone --recursive "https://github.com/hyprwm/Hyprland.git" "${HOME}/.hyprland"
+# We want to build inside a toolbox, but install to the host system. Prefix with a directory that's available both inside and outside.
+(cd "${HOME}/.hyprland"; make install PREFIX="${HOME}/.hyprland-build")
 
 rm -rf \
-    "/tmp/libxcb-errors" \
-    "/tmp/vulkan-renderer" \
-    "/tmp/hyprland"
+    "${HOME}/.hyprland"

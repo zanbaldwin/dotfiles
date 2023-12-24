a38906f0480f3d06ae0e15b6e4701f18f3558115
Zan Baldwin <hello@zanbaldwin.com>
2023-12-24 21:15:26 +0100

    Bash: No Argument Splitting

--- stow/bash/.bashrc.d/docker.sh
+++ stow/bash/.bashrc.d/docker.sh
@@ -6,26 +6,21 @@ if which "docker" >"/dev/null" 2>&1; then
     docker() {
         if [[ ("$1" = "compose") || ("$1" = "composer") ]]; then
             shift
-            # shellcheck disable=SC2068
-            command docker-compose $@
+            command docker-compose "$@"
         elif [[ "$1" = "machine" ]]; then
             shift
-            # shellcheck disable=SC2068
-            command docker-machine $@
+            command docker-machine "$@"
         else
-            # shellcheck disable=SC2068
-            command docker $@
+            command docker "$@"
         fi
     }
 elif command -v "podman" >"/dev/null" 2>&1; then
     docker() {
         if [[ ("$1" = "compose") || ("$1" = "composer") ]]; then
             shift
-            # shellcheck disable=SC2068
-            command docker-compose $@
+            command docker-compose "$@"
         else
-            # shellcheck disable=SC2068
-            command podman $@
+            command podman "$@"
         fi
     }
 fi

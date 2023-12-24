9d3ed8a4d9b94be6dcb0139037996c885c003838
Zan Baldwin <hello@zanbaldwin.com>
2023-12-24 21:15:26 +0100

    Correctly Handle Docker/Compose Aliases under Podman

--- stow/bash/.bashrc.d/docker.sh
+++ stow/bash/.bashrc.d/docker.sh
@@ -3,15 +3,29 @@
 # Old habits die hard...
 alias docker-compose="docker compose"
 alias docker-composer="docker compose"
-if which "docker" >"/dev/null" 2>&1; then
-    docker() {
-        if [[ ("$1" = "composer") ]]; then
-            shift
-            command docker compose "$@"
-        else
-            command docker "$@"
-        fi
-    }
+
+if command -v "docker" >"/dev/null" 2>&1; then
+    if docker version 2>"/dev/null" | grep "Podman" >"/dev/null" 2>&1; then
+        # Docker is symlinked to Podman (probably the "podman-docker" package).
+        docker() {
+            if [[ ("$1" = "compose") || ("$1" = "composer") ]]; then
+                shift
+                command docker-compose "$@"
+            else
+                command docker "$@"
+            fi
+        }
+    else
+        # Actually have Docker installed.
+        docker() {
+            if [[ ("$1" = "composer") || ("$1" = "composer") ]]; then
+                shift
+                command docker compose "$@"
+            else
+                command docker "$@"
+            fi
+        }
+    fi
 elif command -v "podman" >"/dev/null" 2>&1; then
     docker() {
         if [[ ("$1" = "compose") || ("$1" = "composer") ]]; then

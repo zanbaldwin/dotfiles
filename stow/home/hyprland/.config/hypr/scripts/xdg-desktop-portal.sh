bfc5ab171a3b82f41c33b50e793a22488ae09483
Zan Baldwin <hello@zanbaldwin.com>
2023-12-24 21:15:29 +0100

    Update Hyprland Configuration

--- stow/home/hyprland/.config/hypr/scripts/xdg-desktop-portal.sh
+++ stow/home/hyprland/.config/hypr/scripts/xdg-desktop-portal.sh
@@ -1,10 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 sleep 1
-killall -e xdg-desktop-portal-hyprland
-killall -e xdg-desktop-portal-gnome
-killall -e xdg-desktop-portal-wlr
+
+# kill all possible running xdg-desktop-portals
+killall xdg-desktop-portal-hyprland
+killall xdg-desktop-portal-gnome
+killall xdg-desktop-portal-kde
+killall xdg-desktop-portal-lxqt
+killall xdg-desktop-portal-wlr
+killall xdg-desktop-portal-gtk
 killall xdg-desktop-portal
+sleep 1
+
+# start xdg-desktop-portal-hyprland
 /usr/lib/xdg-desktop-portal-hyprland &
 sleep 2
+
+# start xdg-desktop-portal
 /usr/lib/xdg-desktop-portal &
+sleep 1

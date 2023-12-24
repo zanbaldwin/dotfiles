9dadbab7fe179adadbe4cf2b67b533361e2c891f
Zan Baldwin <hello@zanbaldwin.com>
2023-12-24 21:15:25 +0100

    Bash: Sudo Ignore Additional Arguments

--- bash/.bashrc.d/terminal.sh
+++ bash/.bashrc.d/terminal.sh
@@ -16,7 +16,8 @@ export LS_COLORS='ex=00:su=00:sg=00:ca=00:'
 alias c="clear"
 alias xx="exit"
 alias reload=". ~/.bashrc"
-alias sudo="sudo --preserve-env"
+# Most installations of `sudo` have `secure_path` enabled, head to `/etc/sudoers` and remove that line.
+alias sudo="sudo --preserve-env --"
 
 # Watch the output of the last command instead of having to
 # execute it constantly whilst waiting for a different output.

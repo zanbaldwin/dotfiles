c9965705a7f3b318228009eea8777fb471c12d18
Zan Baldwin <hello@zanbaldwin.com>
2023-12-24 21:15:26 +0100

    Get Machine Hardware Name

--- stow/bash/.bashrc.d/silverblue.sh
+++ stow/bash/.bashrc.d/silverblue.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-alias bases='ostree remote refs fedora | grep -v "updates" | grep --no-messages --color="never" "$(uname -i)"'
+alias bases='ostree remote refs fedora | grep -v "updates" | grep --no-messages --color="never" "$(uname -m)"'
 alias switch-base='rpm-ostree rebase --os="fedora" --remote="fedora" --branch'
 alias update='rpm-ostree upgrade --check'
 alias upgrade='rpm-ostree upgrade'

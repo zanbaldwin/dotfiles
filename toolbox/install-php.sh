6e88eff2280b89a15f3e677f6eb3e6459e8c6796
Zan Baldwin <hello@zanbaldwin.com>
2023-12-24 21:15:27 +0100

    PHP: Composer Installer Script Updated

--- toolbox/install-php.sh
+++ toolbox/install-php.sh
@@ -47,13 +47,14 @@ if ! command -v "composer" >"/dev/null" 2>&1; then
 
     sudo dnf install --assumeyes "coreutils"
 
-    COMPOSER_INSTALLER_HASH="55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae"
+    COMPOSER_INSTALLER_HASH="e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02"
     echo "${COMPOSER_INSTALLER_HASH} /tmp/composer-setup.php" >"/tmp/composer-setup.sha384"
     if ! sha384sum --check "/tmp/composer-setup.sha384" --strict --status; then
         echo "Composer installer script has been either been upgraded or tampered with."
         exit 1
     fi
 
+    mkdir -p "${HOME}/bin"
     php "/tmp/composer-setup.php" --install-dir="${HOME}/bin" --filename="composer"
 
     rm "/tmp/composer-setup.php";

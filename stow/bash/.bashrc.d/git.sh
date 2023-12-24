c85814b96ce508ac5dd79735fb1846dc1b871911
Zan Baldwin <hello@zanbaldwin.com>
2023-12-24 21:15:26 +0100

    Update Latest Stable Git Version Bash Function

--- stow/bash/.bashrc.d/git.sh
+++ stow/bash/.bashrc.d/git.sh
@@ -2,7 +2,10 @@
 
 # This function relies on Git repositories having sensible tag names. Do not rely on it.
 latest_stable_version () {
-    local GIT_DIR="${1:-$(pwd)}"
+    #local GIT_DIR="${1:-$(pwd)}"
     # Fetch all tags that contain at least a MAJOR.MINOR, filtering out hyphens (alpha, beta, rc, etc), and print the latest.
-    git -C "${GIT_DIR}" tag --list --sort="version:refname" | grep -E '\d+\.\d+' | grep -v '-' | tail -n1
+    git -C "${GIT_DIR}" tag --list --sort="version:refname" \
+        | rg -e '\d+\.\d+' \
+        | rg -v 'alpha|ALPHA|beta|BETA|rc|RC|-' \
+        | tail -n1
 }

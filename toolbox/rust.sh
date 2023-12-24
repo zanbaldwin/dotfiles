e455951178de8ef8936999d1471ad212993553d9
Zan Baldwin <hello@zanbaldwin.com>
2023-12-24 21:15:29 +0100

    Rust Tools: Restructure What to Install

--- toolbox/rust.sh
+++ toolbox/rust.sh
@@ -24,7 +24,7 @@ command -v "cargo" >"/dev/null" 2>&1 && { \
 } || { \
     curl --proto "=https" --tlsv1.2 -sSf "https://sh.rustup.rs" >"/tmp/rustup.sh" && { \
         sh /tmp/rustup.sh -qy --no-modify-path; \
-        # shellcheck source=/dev/null
+        # shellcheck source="/dev/null"
         source "${HOME}/.cargo/env"; \
     } || { echo >&2 "Error downloading RustLang installation script."; } \
 }
@@ -33,26 +33,29 @@ command -v "cargo" >"/dev/null" 2>&1 && { \
 # Kept running into /tmp ramfs running out of space on small VMs.
 
 # Important Rust binaries I want for *every* installation.
-cargo install "sccache"   # Build Artifact Cache
 cargo install "bat"       # `cat` but better
 cargo install "exa"       # `ls` but better
 cargo install "starship"  # PS1
-
-# Useful Rust binaries that I want on my machine, but I'll cancel the script about here when running in VMs.
-cargo install "coreutils" # Rusty GNU
 cargo install "git-delta" # Better Diffing
-cargo install "mprocs"    # Multi Process Runner
 cargo install "onefetch"  # Git Repository Overview
-cargo install "ripgrep"   # It's just fast
 cargo install "zellij"    # Terminal Multiplexer (tmux replacement)
+cargo install "atuin"         # Better Bash History
+
+# Other tools that I want to try to start using more
+cargo install "gimoji"        # Select an appropriate emoji to Git commits depending on what the commit does
+cargo install "gitui"         # Git TUI
+cargo install "oha"           # HTTP Load Testing
+
+# Useful Rust binaries that I want on my machine, but I'll cancel the script
+# about here when running in VMs.
+cargo install "coreutils" # Rusty GNU
+cargo install "mprocs"    # Multi Process Runner
+cargo install "ripgrep"   # It's just fast
 
 # Control Spotify from command line, instead of through Flatpak. Used for Eww.
 # `secret-tool store --label='spotifyd' application rust-keyring service spotifyd username zanbaldwin`
 cargo install "spotifyd" --features "dbus_keyring"
 cargo install "spotify-tui"
-
-# The following need more testing on Linux. See `stow/bash/.bashrc.d/directories.sh`.
-cargo install "zoxide"    # `cd` alternative
 
 # Specifically for Rust development instead of general tools
 cargo install "bacon"         # Background Rust Code Checker (alternative watch TUI)
@@ -61,10 +64,8 @@ cargo install "cargo-info"    # crates.io in the terminal
 cargo install "tokio-console" # Tokio Tracing Console
 cargo install "cargo-update"  # Update all installed binaries
 
-# Other tools that I want to try to start using more
-cargo install "gimoji"        # Select an appropriate emoji to Git commits depending on what the commit does
-cargo install "gitui"         # Git TUI
-cargo install "oha"           # HTTP Load Testing
-cargo install "atuin"         # Better Bash History
+# The following are kept for reference, but I don't think I want to use them
+#cargo install "zoxide"    # `cd` alternative
+#cargo install "sccache"   # Build Artifact Cache
 
 curl -sSfL "https://get.wasmer.io" | bash

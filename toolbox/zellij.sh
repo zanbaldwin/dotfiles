a8acba630cd894dda4dc6a7d3e134aa313d78419
Zan Baldwin <hello@zanbaldwin.com>
2023-12-24 21:15:29 +0100

    Zellij: Install Plugins Directly after Rust Installation

--- toolbox/zellij.sh
+++ toolbox/zellij.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+if [ -f "${HOME}/.cargo/env" ]; then
+    # Just in case this script is being immediately after `rust.sh` without a reload.
+    # shellcheck source="/dev/null"
+    source "${HOME}/.cargo/env"
+fi
+
 # Zellij Plugins
 zellij_stable_version() {
     GIT_DIR="${1:-$(pwd)}"
     # Fetch all tags that contain at least a MAJOR.MINOR, filtering out hyphens (alpha, beta, rc, etc), and print the latest.
+    # Escape to use the original `grep` so we know what we're getting (just in case `rg` is aliased), and use PCRE2 flag.
     git -C "${GIT_DIR}" tag --list --sort="version:refname" \
-        | rg -e '\d+\.\d+' \
-        | rg -v 'alpha|ALPHA|beta|BETA|rc|RC|-' \
+        | \grep -P -e '\d+\.\d+' \
+        | \grep -P -v 'alpha|ALPHA|beta|BETA|rc|RC|-' \
         | sort --version-sort \
         | tail -n1
 }

dde766ba4c975e6009c52e80119de28b9569289f
Zan Baldwin <hello@zanbaldwin.com>
2023-12-24 21:15:26 +0100

    Add Script for Building Hyprland

---
+++ toolbox/hyprland.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# xcb-errors isn't available in the Fedora repositories.
+sudo dnf install --assumeyes \
+    autoconf \
+    automake \
+    libxcb-devel \
+    xcb-proto \
+    xorg-x11-util-macros
+git clone --recursive "https://gitlab.freedesktop.org/xorg/lib/libxcb-errors" "/tmp/libxcb-errors"
+command -v "python" >"/dev/null" 2>&1 || { sudo ln -s "/usr/bin/python3" "/usr/bin/python"; }
+( \
+    cd "/tmp/libxcb-errors"; \
+    git -C "/tmp/libxcb-errors" checkout "$(latest_stable_version)"; \
+    bash "/tmp/libxcb-errors/autogen.sh" --prefix="/usr"; \
+    sudo make install; \
+)
+
+# Vulkan Renderer
+sudo dnf install --assumeyes \
+    gcc-c++ \
+    glslang-devel \
+    libfontenc-devel \
+    libuuid-devel \
+    libxkbfile-devel \
+    libxcb-devel \
+    libXrandr-devel \
+    libXres-devel \
+    libXaw-devel \
+    libXcursor-devel \
+    libXcomposite-devel \
+    libXdamage-devel \
+    libXdamage-devel \
+    libXdmcp-devel \
+    libXinerama-devel \
+    libXi-devel \
+    libXtst-devel \
+    libXvMC-devel \
+    libXv-devel \
+    libXxf86vm-devel \
+    libXScrnSaver-devel \
+    libICE-devel \
+    libSM-devel \
+    mesa-libGL-devel \
+    mingw64-vulkan-headers \
+    mingw64-vulkan-loader \
+    pip \
+    vulkan-headers \
+    vulkan-loader-devel \
+    xcb-util-devel \
+    xcb-util-image-devel \
+    xcb-util-keysyms-devel \
+    xcb-util-renderutil-devel \
+    xcb-util-wm-devel \
+    xkeyboard-config-devel \
+    xorg-x11-xtrans-devel
+pip install \
+    conan \
+    setuptools \
+    wheel
+git clone --recursive "https://github.com/inexorgame/vulkan-renderer" "/tmp/vulkan-renderer"
+(cd "/tmp/vulkan-renderer"; cmake . -Bbuild -D'CMAKE_BUILD_TYPE=Debug' -G'Ninja' && ninja -C build)
+
+# Hyprland
+sudo dnf install --assumeyes \
+    cairo-devel \
+    cmake \
+    cpio \
+    gcc-c++ \
+    git \
+    hwdata-devel \
+    libavutil-free-devel \
+    libavcodec-free-devel \
+    libavformat-free-devel \
+    libdisplay-info-devel \
+    libdrm-devel \
+    libinput-devel \
+    libliftoff-devel \
+    libseat-devel \
+    libxcb-devel \
+    libxkbcommon-devel \
+    libX11-devel \
+    make \
+    mesa-libEGL-devel \
+    mesa-libgbm-devel \
+    meson \
+    ninja-build \
+    pango-devel \
+    pixman-devel \
+    systemd-devel \
+    wayland-devel \
+    wayland-protocols-devel \
+    wlroots-devel \
+    xcb-util-renderutil-devel \
+    xcb-util-wm-devel \
+    xorg-x11-server-Xwayland-devel
+
+git clone --recursive "https://github.com/hyprwm/Hyperland.git" "/tmp/hyprland"
+(cd "/tmp/hyprland"; make install PREFIX="${HOME}/.hypr")

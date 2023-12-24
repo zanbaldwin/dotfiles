5b89570ba0b04b4e69b0b8d9e33fa255921f60fe
Zan Baldwin <hello@zanbaldwin.com>
2023-12-24 21:15:26 +0100

    Toolbox: Install Wasmer for WebAssembly

--- stow/bash/.bashrc.d/00-paths.sh
+++ stow/bash/.bashrc.d/00-paths.sh
@@ -14,5 +14,9 @@ function add_to_path() {
 
 # Standalone binaries should be put in your home directory (especially for macOS and immutable OSs like Silverblue).
 add_to_path "${HOME}/bin"
+
 # Import Cargo, and all binaries installed by it (whether part of RustLang or just written in Rust)
 [ -f "${HOME}/.cargo/env" ] && . "${HOME}/.cargo/env"
+
+# Import Wasmer, if it's installed.
+[ -s "${HOME}/.wasmer/wasmer.sh" ] && . "${HOME}/.wasmer/wasmer.sh"
--- toolbox/install-rust.sh
+++ toolbox/install-rust.sh
@@ -26,3 +26,43 @@ cargo install "exa"
 cargo install "git-delta"
 cargo install "onefetch"
 cargo install "starship"
+
+curl -sSfL "https://get.wasmer.io" | bash
+
+# This is mainly for reference.
+# Wasmer does not provide signatures for its installer which it expects
+# you to just pipe into shell; this method is for compiling from source.
+install_wasmer_build_from_source () {
+    INSTALL_DIRECTORY="${1:-${HOME}/.cargo/bin}"
+    git clone "https://github.com/wasmerio/wasmer.git" "/tmp/wasmer"
+    cd "/tmp/wasmer" || return
+    # TODO: Find a way to `git checkout "${lATEST_STABLE_VERSION}"`
+    # Release Profile settings taken from Makefile.
+    { \
+        echo "[profile.release]"; \
+        echo "opt-level = 'z'"; \
+        echo "debug = false"; \
+        echo "debug-assertions = false"; \
+        echo "overflow-checks = false"; \
+        echo "lto = true"; \
+        echo "panic = 'abort'"; \
+        echo "incremental = false"; \
+        echo "codegen-units = 1"; \
+        echo "rpath = false"; \
+    } >> "/tmp/wasmer/Cargo.toml"
+    cargo install "xargo"
+    make "build-wasmer"
+    mv "/tmp/wasmer/target/release/wasmer" "${INSTALL_DIRECTORY}/wasmer"
+    # Wasmer Headless requires Rust nightly toolchain.
+    { \
+        echo "[toolchain]"; \
+        echo "channel = \"nightly\""; \
+        echo "components = [ \"rust-src\" ]"; \
+    } > "/tmp/wasmer/rust-toolchain.toml"
+    rm "rust-toolchain"
+    make "build-wasmer-headless-minimal"
+    TARGET="$(rustc -vV | grep 'host:' | cut -d':' -f2 | sed 's/^ *//')"
+    mv "/tmp/wasmer/target/${TARGET}/release/wasmer-headless" "${INSTALL_DIRECTORY}/wasmer-headless"
+    cargo uninstall "xargo"
+    rm -rf "/tmp/wasmer"
+}

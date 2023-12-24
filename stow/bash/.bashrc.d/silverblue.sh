8a045bb10707331d04a51af07a15ed52a5a76d3c
Zan Baldwin <hello@zanbaldwin.com>
2023-12-24 21:15:26 +0100

    Update Bases Alias

--- stow/bash/.bashrc.d/silverblue.sh
+++ stow/bash/.bashrc.d/silverblue.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-alias bases='ostree remote refs fedora | grep --no-messages --color="never" "x86_64/silverblue"'
+alias bases='ostree remote refs fedora | grep -v "updates" | grep --no-messages --color="never" "$(uname -i)"'
 alias switch-base='rpm-ostree rebase --os="fedora" --remote="fedora" --branch'
 alias update='rpm-ostree upgrade --check'
 alias upgrade='rpm-ostree upgrade'

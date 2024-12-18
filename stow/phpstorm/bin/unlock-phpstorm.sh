95b41668bf2234dfb90174b060b530ebea864206
Zan Baldwin <hello@zanbaldwin.com>
2024-12-18 15:51:02 +0100

    20241218T145102Z

--- stow/phpstorm/bin/unlock-phpstorm.sh
+++ stow/phpstorm/bin/unlock-phpstorm.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-cd "${HOME}/.var/app/com.jetbrains.PhpStorm"
-rm --force --verbose config/JetBrains/PhpStorm*/.lock
+
+# For some reason, if I shutdown the computer without exiting PHPStorm first, it
+# leaves behind a lock file that prevents it from being opened the next time I
+# turn on the computer. It's very annoying.
+find "${HOME}/.var/app/com.jetbrains.PhpStorm/config" -type f -name '.lock' -exec rm --force --verbose {} \;
--- toolbox/stow.sh
+++ toolbox/stow.sh
@@ -7,6 +7,7 @@ mkdir -p \
     "${HOME}/code" \
     "${HOME}/.config/autostart" \
     "${HOME}/.config/flakes" \
+    "${HOME}/.config/systemd/user" \
     "${HOME}/.local/share/applications" \
     "${HOME}/.ssh/conf.d" \
     "${HOME}/.ssh/keys"

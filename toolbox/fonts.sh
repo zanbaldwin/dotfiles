3c997f1ba3059109248c501cfbe7d6420114685f
Zan Baldwin <hello@zanbaldwin.com>
2026-02-22 04:07:49 +0100

    Monaspace Fonts

--- toolbox/fonts.sh
+++ toolbox/fonts.sh
@@ -2,6 +2,12 @@
 
 TOOLBOX_SCRIPT_DIRECTORY="$(dirname "$(readlink -f -- "$0")")"
 
+if command -v 'apt-get' >'/dev/null' 2>&1; then
+    sudo apt-get install -y 'xz-utils';
+elif command -v 'dnf' >'/dev/null' 2>&1; then
+    sudo dnf install --assumeyes 'xz';
+fi
+
 mkdir -p "${HOME}/.local/share/fonts"
 find "${TOOLBOX_SCRIPT_DIRECTORY}/../fonts" -mindepth 1 -maxdepth 1 -type f -name "*.tar.xz" \
     -exec tar --extract --verbose --xz \

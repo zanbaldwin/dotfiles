b7a999647dc90168b2d7143abae5e6b6e5834d97
Zan Baldwin <hello@zanbaldwin.com>
2026-03-07 12:10:48 +0100

    Claude

--- stow/bash/.bashrc.d/claude.sh
+++ stow/bash/.bashrc.d/claude.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-alias cc="claude"
+
+if command -v 'claude' >'/dev/null' 2>&1; then
+    cc() {
+        local PR_LINK="${1}"
+        export CLAUDE_CODE_DISABLE_AUTO_MEMORY=0
+        if [ ! -z "${PR_LINK}" ]; then
+            claude --no-chrome --from-pr="${PR_LINK}";
+        else
+            claude --no-chrome;
+        fi
+    }
+fi

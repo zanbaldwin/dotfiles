6dec09b308958744090849c896ae788be1ff53f7
Zan Baldwin <hello@zanbaldwin.com>
2026-06-14 17:53:56 +0200

    Ghostty Build Script

--- toolbox/ghostty.sh
+++ toolbox/ghostty.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 export ZIG_VERSION="0.15.2"
-export GHOSTTY_VERSION="1.3.0"
+export GHOSTTY_VERSION="1.3.1"
 export BUILD_DIR="/tmp/ghostty-build"
 
 mkdir -p "${BUILD_DIR}"
 curl -fsSL "https://ziglang.org/download/${ZIG_VERSION}/zig-$(uname -m)-linux-${ZIG_VERSION}.tar.xz" -o "${BUILD_DIR}/zig.tar.xz"
 curl -fsSL "https://release.files.ghostty.org/${GHOSTTY_VERSION}/ghostty-${GHOSTTY_VERSION}.tar.gz" -o "${BUILD_DIR}/ghostty.tar.gz"
-tar vxJf "${BUILD_DIR}/zig.tar.xz"
-tar vxzf "${BUILD_DIR}/ghostty.tar.gz"
+tar vxJf "${BUILD_DIR}/zig.tar.xz" -C "${BUILD_DIR}"
+tar vxzf "${BUILD_DIR}/ghostty.tar.gz" -C "${BUILD_DIR}"
 
 sudo dnf install --assumeyes \
     'gettext-devel' \
@@ -20,5 +20,5 @@ sudo dnf install --assumeyes \
     'xz'
 
 (cd "${BUILD_DIR}/ghostty-${GHOSTTY_VERSION}"; "${BUILD_DIR}/zig-$(uname -m)-linux-${ZIG_VERSION}/zig" build -Doptimize=ReleaseFast)
-mkdir -p "${HOME}/bin"
-mv "${BUILD_DIR}/ghostty-${GHOSTTY_VERSION}/zig-out/bin/ghostty" "${HOME}/bin"
+mkdir -p "${HOME}/.local/bin"
+mv "${BUILD_DIR}/ghostty-${GHOSTTY_VERSION}/zig-out/bin/ghostty" "${HOME}/.local/bin/ghostty"

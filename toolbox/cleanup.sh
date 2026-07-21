552e99180af0273995b0b046c1190bc50d856295
Zan Baldwin <hello@zanbaldwin.com>
2026-07-21 10:51:53 +0200

    cleanup never cross filesystem boundaries

--- toolbox/cleanup.sh
+++ toolbox/cleanup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-find "${HOME}" -path "${HOME}/.local/share/containers" -prune -o -xtype l -lname '*/.dotfiles/*' -print -exec rm -- '{}' +
+find "${HOME}" -xdev -path "${HOME}/.local/share/containers" -prune -o -xtype l -lname '*/.dotfiles/*' -print -exec rm -- '{}' +
